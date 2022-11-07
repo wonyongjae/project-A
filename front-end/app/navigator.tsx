e03a87b5c2d4aee8b3c0d85a32e3406734045151
wonyongjae <dnjsalsl9182@gmail.com>
2022-11-07 23:52:36 +0900

    MOD menu-list & css apply

--- front-end/app/navigator.tsx
+++ front-end/app/navigator.tsx
@@ -2,7 +2,23 @@ import React, { useState } from "react";
 import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
 import OnOffSwitch from '../components/onOffSwitch';
 
+// 타입 수정
+const NavMenuList = ({menu}:any)=> {
+  return (
+    <Navbar.Content hideIn="xs">
+      {
+        menu.map((item:string, index:number):any => (
+        <Navbar.Link key={index} className="py-2 px-4 rounded-lg m-5 hover:border-purple-400 hover:font-bold hover:border-4" href={`http://localhost:3000/${item}`}>
+          <Text color="secondary" className="hover:font-bold">{item.toUpperCase()}</Text>
+        </Navbar.Link>
+        ))
+      }
+    </Navbar.Content>
+  );
+}
+
 export default function Navigator() {
+  const menu = ['yongjae', 'dashboard', 'gis map', 'settings'];
   const [active, setActive] = useState(false);
 
   const activeHandler = () => {
@@ -12,32 +28,17 @@ export default function Navigator() {
   return (
       <Navbar shouldHideOnScroll isBordered variant="sticky">
         <Navbar.Brand>
-          <Text b color="secondary" hideIn="xs">
-            <a href='/'>
-              LOGO
-            </a>
-          </Text>
+          <Text b color="secondary" hideIn="xs"><a href='/'>LOGO</a></Text>
         </Navbar.Brand>
-        <Navbar.Content hideIn="xs">
-          <Navbar.Link isActive={active} onClick={activeHandler} href="http://localhost:3000/yongjae">Yongjae</Navbar.Link>
-          <Navbar.Link isActive={active} onClick={activeHandler} href="http://localhost:3000/dashboard">DashBoard</Navbar.Link>
-          <Navbar.Link isActive={active} onClick={activeHandler} href="#">GIS MAP</Navbar.Link>
-          <Navbar.Link isActive={active} onClick={activeHandler} href="http://localhost:3000/settings">Settings</Navbar.Link>
-        </Navbar.Content>
+        <NavMenuList menu={menu}/>
         <Navbar.Content>
-          <Navbar.Item>
-            <OnOffSwitch />
-          </Navbar.Item>
-          <Navbar.Item>
-            <Button bordered color="gradient" auto flat as={Link} href="#">
-              Login
-            </Button>
-          </Navbar.Item>
-          <Navbar.Item>
-            <Button bordered color="gradient" auto flat as={Link} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item>
+          <OnOffSwitch />
+          <Button bordered color="gradient" className="hover:border-x-4" auto flat as={Link} href="#">
+            Login
+          </Button>
+          <Button bordered color="gradient" auto flat as={Link} href="#">
+            Sign Up
+          </Button>
         </Navbar.Content>
       </Navbar>
   );
