8638b36685b2fb7cead23ff0023b6c5a6930be61
wonyongjae <dnjsalsl9182@gmail.com>
2022-11-08 23:13:08 +0900

    simple

--- front-end/pages/login/page.tsx
+++ front-end/pages/login/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 import styles from './page.module.css';
 import * as React from 'react';
-import MyButton from '../components/MyButton';
 
-export default function Home() {
+
+
+
+export default function App() {
   return (
     <div className={styles.container}>
     <main className={styles.main}>
-       {/* <h1 className={styles.title} /> */}
-        <MyButton />
+       <h1 className={styles.title}>Login</h1>
+
+        
     </main>
     </div>
   );
