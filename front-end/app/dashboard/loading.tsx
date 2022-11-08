cac9d0aeb3989eb7edeb182135c74bc614dee7dc
wonyongjae <dnjsalsl9182@gmail.com>
2022-11-08 23:14:46 +0900

    MOD loading

--- front-end/app/dashboard/loading.tsx
+++ front-end/app/dashboard/loading.tsx
@@ -1,3 +1,17 @@
-export default function Loading(){
-    return <p>Loading</p>;
+'use client';
+import { Progress, Grid } from "@nextui-org/react";
+
+export default function Loading() {
+  return (
+    <Grid.Container xs={12} sm={6} gap={2}>
+      <Grid>
+        <Progress
+          indeterminated
+          value={50}
+          color="secondary"
+          status="secondary"
+        />
+      </Grid>
+    </Grid.Container>
+  );
 }
