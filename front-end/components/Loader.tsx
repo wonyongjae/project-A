bebdfdae8a0e7441a0494bea574f7944daf642af
wonyongjae <dnjsalsl9182@gmail.com>
2022-11-10 22:16:52 +0900

    MOD simple

--- front-end/components/Loader.tsx
+++ front-end/components/Loader.tsx
@@ -2,16 +2,10 @@
 import { Progress, Grid } from "@nextui-org/react";
 
 export default function Loader() {
-  return (
-    <Grid.Container xs={12} sm={6} gap={2}>
-      <Grid>
-        <Progress
+  return <Progress
           indeterminated
           value={50}
           color="gradient"
           status="secondary"
           />
-      </Grid>
-    </Grid.Container>
-  );
 }
--- front-end/components/Search.tsx
+++ front-end/components/Search.tsx
@@ -1,13 +1,9 @@
 import { Input, Grid } from "@nextui-org/react";
-
 export default function Search() {
   return (
     <Grid.Container gap={4}>
       <Grid>
-        <Input 
-          placeholder="Let's Search"
-          status="secondary"
-        />
+        <Input clearable bordered color="secondary" status="secondary" placeholder="Search" />
       </Grid>
     </Grid.Container>
   );
