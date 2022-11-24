7ec89b82d825f3364b5093cc493c493bdc1083a6
wonyongjae <dnjsalsl9182@gmail.com>
2022-11-24 22:05:41 +0900

    ADD study file

--- back-end-springboot/src/main/java/wyj/backendspringboot/controller/HelloController.java
+++ back-end-springboot/src/main/java/wyj/backendspringboot/controller/HelloController.java
@@ -3,6 +3,11 @@ package wyj.backendspringboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Controller
 public class HelloController{
@@ -10,7 +15,32 @@ public class HelloController{
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!");
-
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter
+    @Setter
+    static class Hello {
+        private String name;
     }
 }
