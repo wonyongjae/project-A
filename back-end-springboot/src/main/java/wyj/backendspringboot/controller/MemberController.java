cf73b63c7502a88d68f076d57a3e64f74c40ec74
wonyongjae <dnjsalsl9182@gmail.com>
2022-11-27 23:58:12 +0900

    ADD member Form & List

--- back-end-springboot/src/main/java/wyj/backendspringboot/controller/MemberController.java
+++ back-end-springboot/src/main/java/wyj/backendspringboot/controller/MemberController.java
@@ -1,8 +1,14 @@
 package wyj.backendspringboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import wyj.backendspringboot.domain.Member;
 import wyj.backendspringboot.service.MemberService;
 
 @Controller
@@ -13,5 +19,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ back-end-springboot/src/main/java/wyj/backendspringboot/controller/MemberForm.java
@@ -0,0 +1,10 @@
+package wyj.backendspringboot.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+    private String name;
+}
